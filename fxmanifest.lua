20da481f17c43c4d0dde613069d9c355cebd0645
SyntaxDev1337 <pisleag.denis2007@gmail.com>
2026-06-11 00:39:17 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Syntax'
+description 'VORP Mute/Unmute System'
+version '7.7.7'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
