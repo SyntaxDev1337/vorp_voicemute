2fcc4873807c4b1976ba1f02d8fbe615c9107303
SyntaxDev1337 <pisleag.denis2007@gmail.com>
2026-06-11 00:42:05 +0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.MutePermission = "vorp.mute"
+
+Config.Messages = {
+    muted        = "~r~You have been muted by an admin.",
+    unmuted      = "~g~You have been unmuted.",
+    mutedAdmin   = "~y~You have a mute on %s (CharID: %s).",
+    unmutedAdmin = "~y~You have been unmuted on %s (CharID: %s).",
+    notFound     = "~r~Player not found.",
+    noPermission = "~r~You do not have permission to use this command.",
+    alreadyMuted = "~r~The player is already muted.",
+    notMuted     = "~r~The player is not muted.",
+}
